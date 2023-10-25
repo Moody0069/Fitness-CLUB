6f8e6f5ee593fe26a94c6424c002f40b55084d3b
Mohamud Moalim <Mohamoudmoalim@yahoo.com>
2023-10-24 22:26:39 -0500

    Fixed the home page, i just need to create a new page

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,55 +1,53 @@
+
 import React from "react";
-import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-// import '.src/components/HomePage/HomePage.css';
+import { useHistory } from "react-router-dom";
 import './HomePage.css';
-import ClassList from "../ClassList/ClassList"
+
 function HomePage() {
   const history = useHistory();
   const classes = [
-    // {
-    //   id: 1,
-    //   name: 'Yoga',
-    //   description: 'A relaxing yoga class to unwind and de-stress.',
-    // },
-    // {
-    //   id: 2,
-    //   name: 'Cycle',
-    //   description: 'An intense cycling workout to boost your cardio fitness.',
-    // },
-    // {
-    //   id: 3,
-    //   name: 'Zumba',
-    //   description: 'A fun and energetic dance fitness class.',
-    // },
+    {
+      id: 1,
+      name: 'Yoga',
+      description: 'A relaxing yoga class to unwind and de-stress.',
+      schedule: 'Mon, Wed, Fri at 7:00 AM',
+    },
+    {
+      id: 2,
+      name: 'Cycle',
+      description: 'An intense cycling workout to boost your cardio fitness.',
+      schedule: 'Tue, Thu, Sat at 5:30 PM',
+    },
+    {
+      id: 3,
+      name: 'Zumba',
+      description: 'A fun and energetic dance fitness class.',
+      schedule: 'Mon and Fri at 6:30 PM',
+    },
   ];
 
-
   return (
-  <div className="container">
-    {/* <h2>Home Page</h2> */}      
+    <div className="container">
       <h1>Welcome to Fitness CLUB</h1>
       <h2>Upcoming Classes</h2>
-
-    
-    <ul>
+      <ul>
         {classes.map((classItem) => (
           <li key={classItem.id}>
-            <button onClick={() => history.push(`/class-detail/${classItem.id}`)}>
-              {classItem.name}
-            </button>
+            <div className="class-card">
+              <h3>{classItem.name}</h3>
+              <p>{classItem.description}</p>
+              <p>Schedule: {classItem.schedule}</p>
+              <button onClick={() => history.push(`/class-detail/${classItem.id}`)}>Learn More</button>
+            </div>
           </li>
         ))}
       </ul>
-
-
-    <button onClick={() => history.push('/SchedulePage')}> Schedule Page </button>
-    <button onClick={() => history.push('/ReservationPage')}> Reservation Page </button>
-{/* <ClassList/> */}
-
-  </div>
-);
+      <div className="cta-buttons">
+        <button onClick={() => history.push('/SchedulePage')}>Schedule Page</button>
+        <button onClick={() => history.push('/ReservationPage')}>Reservation Page</button>
+      </div>
+    </div>
+  );
 }
-    
-      
-    
+
 export default HomePage;
